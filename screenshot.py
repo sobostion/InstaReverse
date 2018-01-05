dac96cb448126b7a9091111d8c8b27814c3481d9
Seb <sebastian@automationlogic.com>
2018-01-05 14:00:33 +0000

    cropped out whitespace and scrollbar

--- screenshot.py
+++ screenshot.py
@@ -3,6 +3,7 @@
 from depot.manager import DepotManager
 from selenium import webdriver
 from pyvirtualdisplay import Display
+from PIL import Image
 
 depot = DepotManager.get()
 display = Display(visible=0, size=(800, 800))  
@@ -11,3 +12,9 @@ driver = webdriver.Chrome('/usr/bin/chromedriver')
 driver.set_window_size(800, 800) # set the window size that you need 
 driver.get('https://github.com')
 driver.save_screenshot('github.png')
+
+# remove scrollbar and whitespace
+
+img = Image.open('github.png')
+cropped_img = img.crop( (0,0,650,650) )
+cropped_img.save('cropped.png')
