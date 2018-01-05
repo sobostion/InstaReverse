4e428706a8b115b0898fb3760f92d177c112aadc
Seb <sebastian@automationlogic.com>
2018-01-05 14:00:33 +0000

    cropped out whitespace and scrollbar

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
-sudo apt-get -y install libxi6 libgconf-2-4 python-pip
+sudo apt-get update
+sudo apt-get -y install libxi6 libgconf-2-4 python-pip xvfb
 sudo pip install pyvirtualdisplay
 sudo pip install selenium
 sudo pip install filedepot
 
+# install pillow
+sudo apt-get -y install libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk python-dev
+sudo pip install pillow
 
 sudo apt-get -y install chromium-chromedriver
 sudo ln -s /usr/lib/chromium-browser/chromedriver /usr/bin/chromedriver
