1d2f366fad0deeb3364a87db29adb854ba3ddb7f
Seb <sebastian@automationlogic.com>
2018-01-10 23:17:56 +0000

    added web form at /contact

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from forms import ContactForm
+from flask_wtf import Form
+from wtforms import TextField
 
 app = Flask(__name__)
+app.secret_key = 'development key'
 
 @app.route('/')
 def index():
         return "<h1>omg it's flask</h1>"
+
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+   form = ContactForm()
+
+   if request.method == 'POST':
+      if form.validate() == False:
+         flash('All fields are required.')
+         return render_template('contact.html', form = form)
+      else:
+         return render_template('success.html')
+   elif request.method == 'GET':
+         return render_template('contact.html', form = form)
 
 if __name__ == "__main__":
         app.run(debug=True)
